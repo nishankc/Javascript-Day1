6b61f2ff6bc914b36a647f0bc050af707a3c3a86
Nishank Chikhalia <nishank@chikhalia.com>
2016-08-09 16:28:40 +0100

    Add

--- scripts/Quiz.js
+++ scripts/Quiz.js
@@ -1,30 +1,34 @@
-var questions = [];
-var answers = [];
 //create an object constructor for the quiz questions and answers
-function question(quest, answer){  
+function createQuestion(question, answer1, answer2, answer3, answer4, answer) {  
     
-    this.quest = quest;
+    this.question = question;
+    this.answer1 = answer1;
+    this.answer2 = answer2;
+    this.answer3 = answer3;
+    this.answer4 = answer4;
     this.answer = answer;
-
-    
+        
 }
 
-//create a question
-var question1 = new question("Question one", "Answer");
-var question2 = new question("Question two", "Answer");
-var question3 = new question("Question three", "Answer");
-var question4 = new question("Question four", "Answer");
-var question5 = new question("Question five", "Answer");
+//create array for question list
+var questionList = [];
+
+//create the questions
+var question1 = new createQuestion("Question 1", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1);
+var question2 = new createQuestion("Question 2", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 2);
+var question3 = new createQuestion("Question 3", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 3);
+var question4 = new createQuestion("Question 4", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 4);
+var question5 = new createQuestion("Question 5", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 2);
+
+questionList.push(question1);
+questionList.push(question2);
+questionList.push(question3);
+questionList.push(question4);
+questionList.push(question5);
 
 
-
-questions = [question1.quest, question2.quest, question3.quest, question4.quest, question5.quest]
-
-
-//output question
-
-for(var i = 0; i < questions.length; i ++){
-
-    document.getElementById("demo").innerHTML += questions[i] + "<br/>";
+for(var i = 0; i < questionList.length; i++){
     
+    document.getElementById("demo").innerHTML += questionList[i].question + "<br/>"; 
 }
+
