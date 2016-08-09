f391201b7e1e1e17904db483ef27f28d4e724aa1
nishankc <nishank@chikhalia.com>
2016-08-09 21:28:05 +0100

    Add Quiz

--- scripts/Quiz.js
+++ scripts/Quiz.js
@@ -1,3 +1,7 @@
+//event listener
+var event1 = document.getElementById("button");
+event1.addEventListener("click",checkAnswers,false)
+
 //create an object constructor for the quiz questions and answers
 function createQuestion(question, answer1, answer2, answer3, answer4, answer) {  
     
@@ -27,8 +31,44 @@ questionList.push(question4);
 questionList.push(question5);
 
 
+
+
+var questIDTag = ["q1", "q2", "q3", "q4", "q5"];
+var ansIDTag = ["a1", "a2", "a3", "a4", "a5"];
+var ansBox = ["ans1", "ans2", "ans3", "ans4", "ans5"];
+var result = ["result1", "result2", "result3", "result4", "result5"];
+
 for(var i = 0; i < questionList.length; i++){
     
-    document.getElementById("demo").innerHTML += questionList[i].question + "<br/>"; 
+    document.getElementById(questIDTag[i]).innerHTML += questionList[i].question + "<br/>"; 
+        
+    document.getElementById(ansIDTag[i]).innerHTML +=     
+        
+        "<ol><li>" + questionList[i].answer1 + "</li><li>" + questionList[i].answer2 + "</li><li>" + questionList[i].answer3 + "</li><li>" + questionList[i].answer4 + "</li></ol>"
+    
 }
 
+function checkAnswers(){
+    
+    countCorrect = 0;
+    countWrong = 0;
+    var userAns = "";
+    
+    for(var i = 0; i < questionList.length; i++){
+        
+        userAns = parseInt(document.getElementById(ansBox[i]).value);
+        
+        if(userAns != questionList[i].answer){
+            
+            //document.getElementById(result[i]).innerHTML += "Incorrect";
+            countWrong++;
+        }else{
+            //document.getElementById(result[i]).innerHTML += "Correct";
+            countCorrect++;
+        }
+        
+    }
+    
+    document.getElementById("display").innerHTML = "Number of Correct Answers: " + countCorrect + "<br/>Number of Incorrect Answers: " + countWrong;
+    
+}
