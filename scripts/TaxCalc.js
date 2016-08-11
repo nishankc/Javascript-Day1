f1080a087c80c0142a90c2062bcb7add594fd3df
nishankc <nishank@chikhalia.com>
2016-08-11 22:02:10 +0100

    Add tax
    taxcalc

--- scripts/TaxCalc.js
+++ scripts/TaxCalc.js
@@ -8,25 +8,68 @@ function taxCalc() {
     var twentyPercent = 20/100; //32k
     var fortyPercent = 40/100; //32-150
     var fortyFivePercent = 45/100; //150+
-    
+    var firstBand = 0;
+    var secondBand = 0;
+    var thirdBand = 0;
+	$('#output').show();
+	$('#output2').show();
+	$('#output3').show();
+    $('#display2').show();
     var orignalGross = parseInt(document.getElementById("gross").value);
     
     if(orignalGross > 150000){
         
-        var firstBand = 32000 * twentyPercent;
-        var secondBand = 118000 * fortyPercent;
-        var thirdband = (orignalGross - 150000) * fortyFivePercent;
-        
-        document.getElementById("display").innerHTML = "Tax at 20%: " + firstBand + "<br/>Tax at 40%: " + secondBand + "<br/>Tax at 45%: " + thirdband;
-        
-    }else if(orignalGross <= 150000){
-        
         firstBand = 32000 * twentyPercent;
-        secondBand = (orignalGross-32000) * fortyFivePercent;
-       
-        document.getElementById("display").innerHTML = "Tax at 20%: " + firstBand + "<br/>Tax at 40%: " + secondBand;
+        secondBand = 118000 * fortyPercent;
+        thirdBand = (orignalGross - 150000) * fortyFivePercent;
+		$('#display2').hide();
+		
+        
+    
+        
+    }else if(orignalGross <= 150000 && orignalGross > 120000){
+		
+		firstBand = 32000  * twentyPercent;
+        secondBand = (orignalGross-32000) * fortyPercent;
+        $('#display2').hide();
+		$('#output3').hide();
+		
+		
+		
+	}else if(orignalGross > 100000 && orignalGross <= 120000){
+		
+		var personalAllowance = (orignalGross-100000)/2;
+		firstBand = (43000 - personalAllowance) * twentyPercent;
+        secondBand = (orignalGross-43000) * fortyPercent;
+        $('#display2').hide();
+		$('#output3').hide();
+		
+	}else if(orignalGross <= 100000 && orignalGross > 43000){
+        
+        firstBand = (43000 - 11000) * twentyPercent;
+        secondBand = (orignalGross-43000) * fortyPercent;
+        $('#display2').hide();
+		$('#output3').hide();
+        
+    }else if(orignalGross <= 43000 && orignalGross > 11000){
+        
+        firstBand = (orignalGross-11000) * twentyPercent;
+		$('#display2').hide();
+		$('#output3').hide();
+		$('#output2').hide();
+        
+    }else if(orignalGross <= 11000){
+        document.getElementById("display2").innerHTML = "No Tax";
+		$('#output').hide();
+		$('#output3').hide();
+		$('#output2').hide();
         
     }
     
+    document.getElementById("output").innerHTML = "Tax at 20%: " + firstBand; 
+	
+    document.getElementById("output2").innerHTML = "<br/>Tax at 40%: " + secondBand;
+		
+	document.getElementById("output3").innerHTML = "<br/>Tax at 45%: " + thirdBand;
     
 }
